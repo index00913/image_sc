2a29d453e84af2ef583ed4d0a01a2ed1eee663a5
index00913 <jisu00913@gmail.com>
2023-09-21 14:34:54 +0900

    이미지 순번에맞는 활성화 처리

--- main.js
+++ main.js
@@ -14,19 +14,25 @@
 //현재수치값 / 전체수치값 * 100 (백분율)
 //현재수치값 / 전체수치값 * 200 (이백분율)
 
+const num = 200;
 const section = document.querySelector('section');
-const imgs = createImgs(section, 200);
-console.log(imgs);
+const imgs = createImgs(section, num);
+
+//activation함수 추가 : 인수로 유사배열, 활성화 순번받음
+//순번에 대한 요소만 보임처리
 
 window.addEventListener('mousemove', (e) => {
+	const percent = getPercent(e, num);
+	activation(imgs, percent);
+});
+
+//이벤트정보 객체와 전체 개수를 받아서
+//해당 숫자에 대한 백분율 반환함수
+function getPercent(e, num) {
 	const curPos = e.pageX;
 	const wid = window.innerWidth;
-	const percent = parseInt((curPos / wid) * 200);
-	console.log(percent);
-
-	//parseInt(숫자) : 실수에서 소수점 아래를 버려서 정수반환
-	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
-});
+	return parseInt((curPos / wid) * 200);
+}
 
 //인수로 객수를 받아서 동적으로 img생성해 주는 함수
 function createImgs(target, num) {
@@ -38,4 +44,11 @@ function createImgs(target, num) {
 		target.append(img);
 	}
 	return target.querySelectorAll('img');
+}
+
+//인수로 그룹유사배열, 활성화 순번을 받아서
+//해당 순번의 요소만 활성화처리
+function activation(arr, index) {
+	arr.forEach((el) => (el.style.display = 'none'));
+	arr[index].style.display = 'block';
 }
